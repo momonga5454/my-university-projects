e40b4cfe59978c062e118be2b9bc3284e7edd581
momonga5454 <liaojinjiujing73@gmail.com>
2025-05-25 09:34:31 +0900

    Delete プログラミング directory

--- プログラミング/503.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-
-char str[]="Hello";
-printf("%s\n",str);
-}
--- プログラミング/504.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-    char str[100];
-    scanf("%s",str);
-    printf(%s\n,str);
-    return 0;
-}
